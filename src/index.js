0b84db71285adab780b5041d81c9c1262a0e2fad
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-02-02 17:04:08 +0700

    feat(products): add API to create and delete products

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 dotenv.config();
 
+app.use(express.json());
+
 const PORT = process.env.PORT;
 
 app.get("/api", (req, res) => {
@@ -17,6 +19,38 @@ app.get("/products", async (req, res) => {
   const products = await prisma.products.findMany();
 
   res.send(products);
+});
+
+app.post("/products", async (req, res) => {
+  const newProductData = req.body;
+
+  const product = await prisma.products.create({
+    data: {
+      name: newProductData.name,
+      price: newProductData.price,
+      description: newProductData.description,
+      image: newProductData.image,
+    },
+  });
+
+  res.send({
+    data: product,
+    message: "Created Product Success",
+  });
+});
+
+app.delete("/product/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  await prisma.products.delete({
+    where: {
+      id: productId,
+    },
+  });
+
+  res.send({
+    message: `Succesful Deleted Product ${productId} `,
+  });
 });
 
 app.listen(PORT, () => {
